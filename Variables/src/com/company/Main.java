463df568a82fd520e2c60f95c94f71f0aa26b94c
apophis775 <apophis775@gmail.com>
2017-09-10 17:16:45 -0500

    Variables

---
+++ Variables/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Byte byte1 = 127;
+        System.out.println("A byte is an 8-bit signed integer which is used to save memory.  This is it's maximum value:" +byte1);
+        Short short1 = 32767;
+        System.out.println("A short is a 16-bit signed integer which is used to save memory.  This is it's maximum value:" +short1);
+        Integer int1 = 24601;
+        System.out.println("An int is a 32 bit signed integer which has a fairly high value.  It's one of the most common variable types.  Here's an example of an int:" +int1);
+        Long long1 = 525600000000000000L;
+        System.out.println("A long is a 64 bit integer.  It can hold an incredebly high value.  Here's an example of a long:" +long1);
+        Float float1 = 3.1415f;
+        System.out.println("A float is a small number with a decimal.  Here's an example:" +float1);
+        Double double1 = 123512353.60124161261235125d;
+        System.out.println("A double is a 64-bit floating point.  This is a really large number with a decimal point.  Here's an example:" +double1);
+        Boolean boolean1 = true;
+        System.out.println("A boolean is a true or false variable.  It holds 1 as true, or 0 as false.  Here's an example: " +boolean1);
+        Character char1 = 'a';
+        System.out.println("A char variable holds a single character.  Here's an example: " +char1);
+
+
+
+
+
+
+
+
+    }
+}
