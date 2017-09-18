2e304f706b5c23c2039e3a24147f9563e800e382
apophis775 <apophis775@gmail.com>
2017-09-17 20:37:31 -0500

    Array Project

---
+++ ArrayProject1/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] menu = new String[5];
+        menu[0]="Hot Dog";
+        menu[1]="Hamburger";
+        menu[2]="Sandwich";
+        menu[3]="Pizza";
+        menu[4]="Soup";
+
+        for(int a = 0;a<5;a++){
+            System.out.println(menu[a]);
+        }
+
+
+    }
+}
