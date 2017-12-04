aa4c21b0a28660f90400b2438157729720356d69
apophis775 <apophis775@gmail.com>
2017-12-03 23:47:42 -0600

    Alpha 0.1
    Minor Update.

--- SS13Clicker/app/src/main/java/com/example/dave/ss13clicker/MainActivity.java
+++ SS13Clicker/app/src/main/java/com/example/dave/ss13clicker/MainActivity.java
@@ -11,25 +11,31 @@ import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGreyTide; //Grey Tide, Station Wide.
+    Button btnWork; //Grey Tide, Station Wide.
     TextView spaceBucks; //Currency TextView
     double bucks = 0; //Currency
-    double greyTiders = 1; //This is added if you have additional grey-tide upgrades purchased.
     double prestige = 1; //This bonus increase each time you "prestige" to a new station.
-
+    String rank = "Assistant"; //Tracks your current rank within a department
+    String dept = "Intern"; //tracks your current department
+    final double base = 1; //This is the "base".  It will NEVER change.
+    double rankMod = 0; //Each time you "rank" up, you receive a bonus.
+    double deptMod = 0; //When you move to a new department, you receive a bonus to work, but your Rank resets.
+    double bonus = 0; //This is a prestige bonus;  When you transfer to a new station, you start as an Assistant again, but you keep 10% of the bonus from your Rank and Department at time of transfer.  It also compounds.
+    double upgrade = 0; //This is the Upgrade bonus, tracked by the tools you have.  These bonuses stay with you until you transfer to another station, then they are lost.
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnGreyTide = (Button)findViewById(R.id.btnGreyTide);
+        btnWork = (Button)findViewById(R.id.btnGreyTide);
         spaceBucks = (TextView)findViewById(R.id.txtCurrency);
 
-        btnGreyTide.setOnClickListener(new View.OnClickListener() {
+
+        btnWork.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bucks+= (greyTiders*prestige);
+                bucks+= base+(1*(rankMod+deptMod+bonus+upgrade)); //Always adds at least 1 "work" to the total, but allows bonuses to add more.
                 spaceBucks.setText(String.valueOf(bucks));
             }
         });
