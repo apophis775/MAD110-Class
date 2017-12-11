518813826f8e338b6e2bd82609aa10c3f5ce7dba
apophis775 <apophis775@gmail.com>
2017-12-10 23:59:56 -0600

    Clicker 0.3

--- SS13Clicker/app/src/main/java/com/example/dave/ss13clicker/MainActivity.java
+++ SS13Clicker/app/src/main/java/com/example/dave/ss13clicker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dave.ss13clicker;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,17 +16,33 @@ public class MainActivity extends AppCompatActivity {
     Button btnWork; //Grey Tide, Station Wide.
     Button btnUpgrade;//Upgrade Menu
     TextView spaceBucks; //Currency
-    TextView department; //Department
     TextView rank; //Rank
     double bucks = 0; //Currency
-    double prestige = 1; //This bonus increase each time you "prestige" to a new station.
     String rankS = "Assistant"; //Tracks your current rank within a department
-    String deptS = "Intern"; //tracks your current department
     final double base = 1; //This is the "base".  It will NEVER change.
     double rankMod = 0; //Each time you "rank" up, you receive a bonus.
-    double deptMod = 0; //When you move to a new department, you receive a bonus to work, but your Rank resets.
     double bonus = 0; //This is a prestige bonus;  When you transfer to a new station, you start as an Assistant again, but you keep 10% of the bonus from your Rank and Department at time of transfer.  It also compounds.
     double upgrade = 0; //This is the Upgrade bonus, tracked by the tools you have.  These bonuses stay with you until you transfer to another station, then they are lost.
+    double perSecondBonus = 0;
+    CountDownTimer timer;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                bucks = data.getExtras().getDouble("bucks");
+                spaceBucks.setText(String.valueOf(bucks));
+
+
+            }
+        }
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +52,28 @@ public class MainActivity extends AppCompatActivity {
         btnWork = (Button)findViewById(R.id.btnGreyTide);
         btnUpgrade = (Button)findViewById(R.id.btnUpgrade);
         spaceBucks = (TextView)findViewById(R.id.txtCurrency);
-        department = (TextView)findViewById(R.id.txtDepartment);
         rank = (TextView)findViewById(R.id.txtRank);
+
+
+        //TIMER FOR TIMED PRODUCTION UPDATES
+        timer = new CountDownTimer()
+
+
+
 
         //Initial Sets
         spaceBucks.setText(String.valueOf(bucks));
-        department.setText(deptS);
         rank.setText(rankS);
 
 
         btnWork.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bucks+= base+(1*(rankMod+deptMod+bonus+upgrade)); //Always adds at least 1 "work" to the total, but allows bonuses to add more.
+                bucks+= base+(1*(rankMod+bonus+upgrade)); //Always adds at least 1 "work" to the total, but allows bonuses to add more.
                 spaceBucks.setText(String.valueOf(bucks));
             }
         });
+
 
 
         btnUpgrade.setOnClickListener(new View.OnClickListener(){
@@ -58,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent nextScreen = new Intent(MainActivity.this, Upgrades.class);
                 nextScreen.putExtra("bucks", bucks);
-                startActivity(nextScreen);
+                startActivityForResult(nextScreen,1);
             }
         });
 
--- SS13Clicker/app/src/main/java/com/example/dave/ss13clicker/Upgrades.java
+++ SS13Clicker/app/src/main/java/com/example/dave/ss13clicker/Upgrades.java
@@ -2,22 +2,58 @@ package com.example.dave.ss13clicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 import android.content.Intent;
 
 public class Upgrades extends AppCompatActivity {
     TextView spaceBucks; //Currency
-
-
+    Button btnBack;
+    Button upgrade1;
+    double bucks;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        double bucks = getIntent().getExtras().getDouble("bucks");
+        bucks = getIntent().getExtras().getDouble("bucks");
         setContentView(R.layout.activity_upgrades);
         spaceBucks = (TextView)findViewById(R.id.txtWork);
         spaceBucks.setText(String.valueOf(bucks));
+        btnBack = (Button)findViewById(R.id.btnBack);
+        upgrade1 = (Button)findViewById(R.id.btnGreyUp);
+
+
+
+        upgrade1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                bucks++;
+            }
+        });
+
+
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("bucks",bucks);
+                setResult(MainActivity.RESULT_OK,returnIntent);
+                finish();
+
+                /*Intent mainScreen = new Intent(Upgrades.this, MainActivity.class);
+                startActivity(mainScreen);*/
+            }
+        });
+
+
+
+
+
+
+
     }
 }
